74131d96f9345ac406d1b8fced80a8082c9b6e88
bli23-01 <hust_hjj@126.com>
2021-07-11 21:51:24 +0800

    coinPool对象池创建

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, NodePool, Prefab, instantiate, CCInteger, math, randomRangeInt } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
@@ -10,14 +10,96 @@ export class GameManager extends Component {
     // [2]
     // @property
     // serializableDummy = 0;
+    @property(
+        { type: Prefab }
+    )
+    coinPerfab: Prefab = null!;
+    @property(
+        { type: CCInteger }
+    )
+    coinPoolNumber = 20;
+    
+    @property(
+        { type: Prefab }
+    )
+    barPerfab: Prefab = null!;
+    @property(
+        { type: CCInteger }
+    )
+    barPoolNumber = 5;
 
-    start () {
-        // [3]
+    @property(
+        { type: Node }
+    )
+    player: Node = null!;
+    @property(
+        { type: Node }
+    )
+    coinRoot: Node = null!;
+    @property(
+        { type: Node }
+    )
+    barRoot: Node = null!;
+
+    playerCurentZ = 0;
+    playerLastZ = 0;
+    private _coinPool: NodePool = new NodePool();
+    private _barPool: NodePool = new NodePool();
+    start()
+    {
+        for (let i = 0; i < this.coinPoolNumber; ++i)
+        {
+            let coin = instantiate(this.coinPerfab);
+            this._coinPool.put(coin);
+        }
+        for (let i = 0; i < this.barPoolNumber; ++i)
+        {
+            let bar = instantiate(this.barPerfab);
+            this._barPool.put(bar);
+        }
+        for (let i = 0; i < this.coinPoolNumber - 5; ++i)
+        {
+            let coin = this._coinPool.get();
+            if (coin != null)
+            {
+                coin.parent = this.coinRoot;
+                coin.setPosition(new math.Vec3(randomRangeInt(-1, 1) * 1.5, 0.7, 5 * i));
+            }
+        }
     }
 
-    // update (deltaTime: number) {
-    //     // [4]
-    // }
+    update(deltaTime: number)
+    {
+        this.playerCurentZ += this.player.position.z - this.playerLastZ;
+        this.playerLastZ = this.player.position.z;
+        if (this.playerCurentZ > 30)
+        {
+            this.playerCurentZ -= 30;
+            let childLenth = this.coinRoot.children.length;
+            let tempZ = this.player.position.z;
+            let killNumber = 0;
+            for (let i = childLenth - 1; i >= 0; --i)
+            {
+                console.log("1." + this.coinRoot.children[i].position.z);
+                console.log("2." + tempZ);
+                if (this.coinRoot.children[i].position.z < tempZ - 5)
+                {
+                    this._coinPool.put(this.coinRoot.children[i]);
+                    killNumber++;
+                }
+            }
+            console.log(killNumber+"?");
+            for (let i = 0; i < killNumber; ++i)
+            {
+                let coin = this._coinPool.get();
+                if (coin != null)
+                {
+                    coin.parent = this.coinRoot;
+                    coin.setPosition(new math.Vec3(randomRangeInt(-1, 1) * 1.5, 0.7, 25 + tempZ + 5 * i));
+                }
+            }
+        }
+    }
 }
 
 /**
